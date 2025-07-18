d36a1d4efeb5e41c6325fdf96bd60aaaf430fdae
kallenlonbom <lonbom@chapman.edu>
2025-07-17 20:20:13 -0700

    added treats saved value

--- src/app/src/main/java/edu/chapman/monsutauoka/services/StepCounterService.kt
+++ src/app/src/main/java/edu/chapman/monsutauoka/services/StepCounterService.kt
@@ -9,14 +9,18 @@ class StepCounterService(val dataStore: DataStore) {
     private var previousCount: Float? = null
 
     private val _steps: MutableStateFlow<Float>
+    private val _treats: MutableStateFlow<Float>
     val steps: StateFlow<Float> get() = _steps
+    val treats: StateFlow<Float> get() = _treats
 
     init {
         // "StepCounterService.steps"
         key = "${this::class.simpleName}.${::steps.name}"
 
         var stepsValue = dataStore.load(key)?.toFloatOrNull()
+        var treatsValue = dataStore.load(key)?.toFloatOrNull()
         _steps = MutableStateFlow(stepsValue ?: 0f)
+        _treats = MutableStateFlow(treatsValue ?: 0f)
     }
 
     fun updateSteps(newCount: Float) {
@@ -27,6 +31,11 @@ class StepCounterService(val dataStore: DataStore) {
 
         _steps.value += newCount - previousCount!!
         previousCount = newCount
+
+        if (_steps.value > 100){
+            _steps.value -= 100
+            _treats.value += 1
+        }
 
         dataStore.save(key, _steps.value.toString())
     }
